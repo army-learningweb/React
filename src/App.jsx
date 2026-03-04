8793a2726113e621b145b104a10c0bbf59e7e371
luuducvy <luuvy15899@gmail.com>
2026-03-04 16:54:15 +0700

    import export

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-// Component
-
-function Welcome() {
-  return (<h1>Xin chào bạn - tôi là component Welcome</h1>);
-}
+import {Welcome} from "./components/Welcome";
 
 function App() {
   return (
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,5 @@
+function Welcome() {
+  return <h1>Xin chào bạn - tôi là component Welcome</h1>;
+}
+
+export {Welcome};
