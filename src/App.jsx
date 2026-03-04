bca9859cf704634067c674d4392419a8293319d9
luuducvy <luuvy15899@gmail.com>
2026-03-04 14:50:35 +0700

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Vite + React (Change)</h1>
       <p>
         Click on the Vite and React logos to learn more
       </p>
