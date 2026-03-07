8a515df32b3f25f798e0dfb9546b989dad465cde
luuducvy <luuvy15899@gmail.com>
2026-03-07 14:05:21 +0700

    Cập nhật

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-// React
-import { useState } from "react";
-
-// SVG
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 // Component
 import { UserProfile } from "./components/UserProfile";
-import { Welcome } from "./components/Welcome";
-import { Child } from "./components/Child";
 
 // App
 function App() {
+
+  // logic
+  const user = {
+    name: "Lưu Đức Vỹ",
+    avatar: "Vỹ",
+    skill: ["HTML,CSS,JS", "PHP,LARAVEL"],
+  };
+
+  const user2 = {
+    name: "Lưu Đức Vỹ",
+    avatar: "Vỹ",
+    skill: ["HTML,CSS,JS", "PHP,LARAVEL"],
+  };
+
   return (
     <>
-      <UserProfile name="Lưu Đức Vỹ" avatar = "Vỹ" skill = {['HTML,CSS','PHP,LARAVEL']}/>
+      <UserProfile user={user}/>
+      <UserProfile user={user2}/>
     </>
   );
 }
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,17 +1,6 @@
 import "../UserProfile.css";
 
-function UserProfile({name,avatar,skill}) {
-  // Logic
-  // const name = 'Lưu Đức Vỹ'
-  // const avatar = "Vỹ"
-  // const skill = ['HTML,CSS,JS','PHP,LARAVEL'];
-
-  // const user = {
-  //   name: "Lưu Đức Vỹ",
-  //   avatar: "Vỹ",
-  //   skill: ["HTML,CSS,JS", "PHP,LARAVEL"],
-  // };
-
+function UserProfile({ user }) {
   // HTML
   return (
     <div
@@ -21,14 +10,15 @@ function UserProfile({name,avatar,skill}) {
         color: "white",
         padding: "15px",
         borderRadius: "15px",
+        marginTop: "5px"
       }}
     >
-      <div className="name">{name}</div>
-      <div className="avatar">{avatar}</div>
+      <div className="name">{user.name}</div>
+      <div className="avatar">{user.avatar}</div>
       <div className="skill">
         <ul>
-            <li>{skill[0]}</li>
-            <li>{skill[1]}</li>
+            <li>{user.skill[0]}</li>
+            <li>{user.skill[1]}</li>
         </ul>
       </div>
     </div>
